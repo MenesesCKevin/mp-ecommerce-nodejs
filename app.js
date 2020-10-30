4aa8bd96ce03111bfe65cc6883e0844eb7fa5575
Kevin Meneses <k3.22@live.com.mx>
2020-10-29 20:21:10 -0600

    c

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', req.query);
+});
+
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+
+const PORT = process.env.PORT || 5000;
+ 
+app.listen(PORT, ()=> console.log("Server start on port 5000"));
